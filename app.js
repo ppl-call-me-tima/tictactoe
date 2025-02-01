525231340da5ec2f0d903e145628cf6296bac21f
Amit Upadhyay <2648amitupadhyay@gmail.com>
2025-02-01 15:39:41 +0530

    Add express-ejs-layout

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-const express = require("express")
-const path = require("path")
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const path = require("path");
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "layouts/main");
 
 app.get("/", (req, res) => {
-    res.render("index"  );
-})
+    res.render("index");
+});
+
+app.get("/play", (req, res) => {
+    res.render("play")
+});
 
 app.listen(port, () => {
     console.log("listening on port:", port)
